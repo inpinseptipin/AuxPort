e9570017c155906909823e783eb48d6c5c02a357
inpinseptipin <sxa1654@miami.edu>
2024-01-03 09:48:28 -0500

    Restructured Fast Fourier Transform Module

---
+++ AuxPort/Audio/FFT/AuxFFT.cpp
@@ -0,0 +1,92 @@
+#include "AuxFFT.h"
+
+AuxPort::Audio::FourierTransform::FourierTransform(size_t fftSize)
+{
+	uint32 isPowerOfTwo = (uint32)(fftSize & (fftSize - 1));
+	AuxAssert(isPowerOfTwo==0, "Library is compatible for FFT sizes that are multiples of 2");
+	_fftValues.resize(fftSize);
+	_log2N = uint32(log2(fftSize));
+}
+
+void AuxPort::Audio::FourierTransform::computeTransform(const std::vector<float> inputBuffer, std::vector<float>& outputBuffer)
+{
+	AuxAssert(inputBuffer.size() == _fftValues.size(), "Size of input buffer not same as the FFT Engine's expectation");
+	AuxAssert(outputBuffer.size() == inputBuffer.size(), "FFT output buffer's size should be the same as the input audio buffer");
+	for (uint32_t i = 0; i < inputBuffer.size(); i++)
+		_fftValues[i] = inputBuffer[i];
+	compute();
+	for (uint32_t i = 0; i < inputBuffer.size(); i++)
+		outputBuffer[i] = _fftValues[i].real();
+}
+
+void AuxPort::Audio::FourierTransform::computeInverseTransform(std::vector<float>& outputBuffer)
+{
+	for (uint32_t i = 0; i < _fftValues.size(); i++)
+		_fftValues[i] = std::conj(_fftValues[i]);
+	compute();
+	for (uint32_t i = 0; i < _fftValues.size(); i++)
+		_fftValues[i] = std::conj(_fftValues[i]);
+	for (uint32_t i = 0; i < _fftValues.size(); i++)
+		outputBuffer[i] = _fftValues[i].real();
+}
+
+std::complex<float> AuxPort::Audio::FourierTransform::get(size_t index) const
+{
+	return _fftValues[index];
+}
+
+size_t AuxPort::Audio::FourierTransform::size() const
+{
+	return size_t();
+}
+
+void AuxPort::Audio::FourierTransform::compute()
+{
+	std::complex<float> temp;
+	uint32_t N = static_cast<uint32_t>(_fftValues.size());
+	uint32_t K = N;
+	uint32_t n;
+	float theta = static_cast<float>(pi / N);
+	std::complex<float> phi = std::complex<float>(cos(theta), -sin(theta));
+	std::complex<float> T = 1.0L;
+
+	while (K > 0)
+	{
+		n = K;
+		K >>= 1;
+		phi = phi * phi;
+		T = 1.0L;
+		for (uint32_t i = 0; i < K; i++)
+		{
+			for (uint32_t j = i; j < N; j += n)
+			{
+				uint32_t b = j + K;
+				temp = _fftValues[j] - _fftValues[b];
+				_fftValues[j] += _fftValues[b];
+				_fftValues[b] = temp * T;
+			}
+			T *= phi;
+		}
+	}
+	int32_t numberOfBits = (int32_t)log2(N);
+	for (uint32_t i = 0; i < N; i++)
+	{
+		uint32_t j = reverseBits(i);
+		if (j > i)
+		{
+			temp = _fftValues[i];
+			_fftValues[i] = _fftValues[j];
+			_fftValues[j] = temp;
+		}
+	}
+}
+
+uint32 AuxPort::Audio::FourierTransform::reverseBits(uint32 b)
+{
+	b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
+	b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
+	b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
+	b = (((b & 0xff00ff00) >> 8) | ((b & 0x00ff00ff) << 8));
+	b = ((b >> 16) | (b << 16)) >> (32 - _log2N);
+	return b;
+}
--- AuxPort/Audio/FFT/AuxFFT.h
+++ AuxPort/Audio/FFT/AuxFFT.h
@@ -58,157 +58,39 @@ namespace AuxPort
 			///////////////////////////////////////////////////////////////////////////////////////
 			/// [Constructor] Provide FFT Size, (Note should be a power of 2).
 			///////////////////////////////////////////////////////////////////////////////////////
-			FourierTransform(const size_t& fftSize)
-			{
-				uint32 isPowerOfTwo = (uint32)(fftSize & (fftSize - 1));
-				if (isPowerOfTwo == 0)
-				{
-					_fftValues.resize(fftSize);
-					_log2N = uint32(log2(fftSize));
-				}
-			}
+			FourierTransform(size_t fftSize);
 
 			///////////////////////////////////////////////////////////////////////////////////////
 			/// [Function] Performs FFT on the AudioBuffer passed in the argument and returns an AudioBuffer with FFT Values.
 			///////////////////////////////////////////////////////////////////////////////////////
-			template<class sample>
-			void computeTransform(const AuxPort::Audio::Buffer<sample>& buffer, AuxPort::Audio::Buffer<sample>& output)
-			{
-				if (buffer.size() == _fftValues.size())
-				{
-					for (uint32 i = 0; i < buffer.size(); i++)
-					{
-						_fftValues[i] = buffer.get(i);
-					}
-				}
-				compute();
-				if (output.size() != buffer.size())
-					output.resize(buffer.size());
-				std::complex<sample> temp;
-				for (uint32 i = 0; i < buffer.size(); i++)
-				{
-					temp = _fftValues[i];
-					output.set(temp.real(), i);
-				}
-			}
-
-			///////////////////////////////////////////////////////////////////////////////////////
-			/// [Function] Performs FFT on the AudioBuffer passed in the argument and returns an AudioBuffer with FFT Values.
-			///////////////////////////////////////////////////////////////////////////////////////
-			template<class sample>
-			void computeTransform(const AuxPort::Audio::Buffer<sample>& buffer)
-			{
-				if (buffer.size() == _fftValues.size())
-				{
-					for (uint32 i = 0; i < buffer.size(); i++)
-					{
-						_fftValues[i] = buffer.get(i);
-					}
-				}
-				compute<float>();
-			}
-
+			void computeTransform(const std::vector<float> inputBuffer, std::vector<float>& output);
 			///////////////////////////////////////////////////////////////////////////////////////
 			/// [Function] Performs in place IFFT
 			///////////////////////////////////////////////////////////////////////////////////////
-			template<class sample>
-			void computeInverseTransform(AuxPort::Audio::Buffer<sample>& output)
-			{
-				for (size_t i = 0; i < _fftValues.size(); i++)
-				{
-					_fftValues[i] = std::conj(_fftValues[i]);
-				}
-				compute();
-				for (size_t i = 0; i < _fftValues.size(); i++)
-				{
-					_fftValues[i] = std::conj(_fftValues[i]);
-				}
-				std::complex<sample> norm((sample)_fftValues.size(), 0);
-				for (size_t i = 0; i < _fftValues.size(); i++)
-				{
-					_fftValues[i] /= norm;
-				}
-				std::complex<sample> temp;
-				for (uint32 i = 0; i < _fftValues.size(); i++)
-				{
-					temp = _fftValues[i];
-					output.set(temp.real(), i);
-				}
-			}
-
-
-			template<class sample>
-			std::complex<sample> get(const size_t& index) const
-			{
-				return _fftValues[index];
-			}
-
-			size_t size() const
-			{
-				return _fftValues.size();
-			}
+			void computeInverseTransform(std::vector<float>& outputBuffer);
+			///////////////////////////////////////////////////////////////////////////////////////
+			/// [Function] Performs in-place FFT
+			///////////////////////////////////////////////////////////////////////////////////////
+			std::complex<float> get(size_t index) const;
+			///////////////////////////////////////////////////////////////////////////////////////
+			/// [Function] Returns the size
+			///////////////////////////////////////////////////////////////////////////////////////
+			size_t size() const;
 		private:
 
 			///////////////////////////////////////////////////////////////////////////////////////
 			/// [Function] Performs in-place FFT
 			///////////////////////////////////////////////////////////////////////////////////////
-			template<class sample>
-			void compute()
-			{
-				std::complex<sample> temp;
-				uint32 N = (uint32)_fftValues.size();
-				uint32 K = N;
-				uint32 n;
-				sample theta = static_cast<sample>(pi / N);
-				std::complex<sample> phi = std::complex(cos(theta), -sin(theta));
-				std::complex<sample> T = 1.0L;
-
-				while (K > 0)
-				{
-					n = K;
-					K >>= 1;
-					phi = phi * phi;
-					T = 1.0L;
-					for (uint32 i = 0; i < K; i++)
-					{
-						for (uint32 j = i; j < N; j += n)
-						{
-							uint32 b = j + K;
-							temp = _fftValues[j] - _fftValues[b];
-							_fftValues[j] += _fftValues[b];
-							_fftValues[b] = temp * T;
-						}
-						T *= phi;
-					}
-				}
-				int noOfBits = (int)log2(N);
-				for (uint32 i = 0; i < N; i++)
-				{
-					uint32 j = reverseBits(i);
-					if (j > i)
-					{
-						temp = _fftValues[i];
-						_fftValues[i] = _fftValues[j];
-						_fftValues[j] = temp;
-					}
-
-				}
-			}
-
-
-			uint32 reverseBits(uint32 b)
-			{
-				b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
-				b = (((b & 0xcccccccc) >> 2) | ((b & 0x33333333) << 2));
-				b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
-				b = (((b & 0xff00ff00) >> 8) | ((b & 0x00ff00ff) << 8));
-				b = ((b >> 16) | (b << 16)) >> (32 - _log2N);
-				return b;
-			}
+			void compute();
+			
+			///////////////////////////////////////////////////////////////////////////////////////
+			/// [Function] Reverse Bits
+			///////////////////////////////////////////////////////////////////////////////////////
+			uint32 reverseBits(uint32 b);
 
 			std::vector<std::complex<float>> _fftValues;
 
-			uint32 _log2N;
+			uint32 _log2N = 0;
 		};
 	}
 }
