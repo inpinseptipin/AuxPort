1cf41b7bfecdc12ca0f296cf6db1102d3c1f9db0
Nitin Rawat <nitin.rawat@asu.apeejay.edu>
2024-02-15 02:16:04 +0530

    Added AuxCurl Extension Module

---
+++ Extensions/AuxCurl/AuxCurl.cpp
@@ -0,0 +1,54 @@
+#include "AuxCurl.h"
+
+Extensions::AuxCurl::AuxCurl()
+{
+	curlHandle = curl_easy_init();
+	AuxAssert(curlHandle, "Error creating CURL Handle!");
+}
+
+Extensions::AuxCurl::~AuxCurl()
+{
+	curl_easy_cleanup(curlHandle);
+}
+
+bool Extensions::AuxCurl::GET(const std::string& URL, std::string& response)
+{
+	curl_easy_setopt(curlHandle, CURLOPT_URL, URL.data());
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeCallback);
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void*)&response);
+
+	CURLcode res = curl_easy_perform(curlHandle);
+
+	if (res != CURLE_OK)
+	{
+		response = "Error in curl_easy_perform(): ";
+		response += curl_easy_strerror(res);
+		return false;
+	}
+	return true;
+}
+
+bool Extensions::AuxCurl::POST(const std::string& URL, const std::string& postFields, std::string& response)
+{
+	curl_easy_setopt(curlHandle, CURLOPT_URL, URL.data());
+	curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, postFields.data());
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeCallback);
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void*)&response);
+
+	CURLcode res = curl_easy_perform(curlHandle);
+
+	if (res != CURLE_OK)
+	{
+		response = "Error in curl_easy_perform(): ";
+		response += curl_easy_strerror(res);
+		return false;
+	}
+	return true;
+}
+
+size_t Extensions::AuxCurl::writeCallback(void* data, size_t size, size_t nmemb, void* clientp)
+{
+	std::string* responseStringPtr = (std::string*)clientp;
+	responseStringPtr->append((const char*)data, size * nmemb);
+	return size * nmemb;
+}
