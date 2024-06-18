61db1d7b497bac900f4a29ce8ad7e2f103429e71
inpinseptipin <saty@satyartharora.com>
2024-06-19 03:56:17 +0530

    Interpolation and Statistical Error

---
+++ AuxPort/Core/Utility/AuxMath.cpp
@@ -0,0 +1,163 @@
+#include "AuxMath.h"
+
+AuxPort::Interpolation::Interpolation()
+{
+	this->type = Type::Linear;
+	startX = endX = delta = valuesCount = 0;
+}
+
+void AuxPort::Interpolation::setType(Type type)
+{
+	this->type = type;
+}
+
+void AuxPort::Interpolation::setXValues(float start, float end, float delta)
+{
+	AuxAssert(start < end, "Start should be less than end!");
+	AuxAssert(end >= start + delta, "There should be atleast 2 values for interpolation!");
+	AuxAssert(delta > 0, "Delta should be positive (greater than 0)!");
+	this->startX = start;
+	this->endX = end;
+	this->delta = delta;
+	this->valuesCount = (end - start) / delta + 1;
+}
+
+void AuxPort::Interpolation::setYValues(const std::vector<float> yValues)
+{
+	AuxAssert(valuesCount != 0, "No xValues provided. Call setXValues() first!");
+	AuxAssert(yValues.size() == valuesCount, "Number of yValues must be equal to number of xValues!");
+	this->yValues = yValues;
+}
+
+float AuxPort::Interpolation::polate(float val)
+{
+	AuxAssert(valuesCount != 0, "No xValues provided. Call setXValues() first!");
+	AuxAssert(yValues.size() == valuesCount, "Number of yValues must be equal to number of xValues!");
+	AuxAssert(val >= startX && val <= endX, "val should be in the range [start, end]");
+	switch (type)
+	{
+	case Type::Linear:
+		return linearInterpolate(val);
+	case Type::Cubic:
+		return cubicInterpolate(val);
+	case Type::Lagrange:
+		return lagrangeInterpolate(val);
+	case Type::Cosine:
+		return cosineInterpolate(val);
+	case Type::Newton:
+		return newtonInterpolate(val);
+	}
+}
+
+float AuxPort::Interpolation::linearInterpolate(float val)
+{
+	AuxAssert(val >= startX && val <= endX, "Given val should lie in the range [start, end]!");
+	size_t index = findIndex(val);
+	
+	float x0 = startX + index * delta;
+	float x1 = x0 + delta;
+	float y0 = yValues[index];
+	float y1 = yValues[index + 1];
+	
+	return y0 + (val - x0) * (y1 - y0) / (x1 - x0);
+}
+
+float AuxPort::Interpolation::cubicInterpolate(float val)
+{
+	AuxAssert(val >= startX && val <= endX, "Given val should lie in the range [start, end]!");
+	size_t index = findIndex(val);
+
+	float x1 = startX + index * delta;
+	float x2 = x1 + delta;
+	float x0 = (index == 0 ? startX : x1 - delta);
+	float x3 = (index + 2 < valuesCount ? x2 + delta : x2);
+
+	float y0 = yValues[index == 0 ? 0 : index - 1];
+	float y1 = yValues[index];
+	float y2 = yValues[index + 1];
+	float y3 = yValues[index + 2 < valuesCount ? index + 2 : index + 1];
+
+	// Finding coefficients of cubic polynomial curve between x1 and x2
+	float a = (-y0 + 3 * y1 - 3 * y2 + y3) / 2;
+	float b = y0 - (2.5f * y1) + 2 * y2 - (y3 / 2);
+	float c = (-y0 + y2) / 2;
+	float d = y1;
+
+	// Normalized val between x1 and x2
+	float nVal = (val - x1) / delta;
+	return a * nVal * nVal * nVal + b * nVal * nVal + c * nVal + d;
+}
+
+float AuxPort::Interpolation::cosineInterpolate(float val)
+{
+	AuxAssert(val >= startX && val <= endX, "Given val should lie in the range [start, end]!");
+	size_t index = findIndex(val);
+
+	float x0 = startX + index * delta;
+	float y0 = yValues[index];
+	float y1 = yValues[index + 1];
+
+	// Normalized between x0 and x1
+	float mu = (val - x0) / delta;
+	float mu2 = (1 - cos(mu * AuxPort::pi)) / 2;
+	return y0 * (1 - mu2) + y1 * mu2;
+}
+
+float AuxPort::Interpolation::lagrangeInterpolate(float val)
+{
+	AuxAssert(val >= startX && val <= endX, "Given val should lie in the range [start, end]!");
+
+	float res = 0.0f;
+	float currTerm;
+	float currX;
+	float xi = startX;
+	for (int i = 0; i < valuesCount; i++)
+	{
+		currTerm = yValues[i];
+		
+		currX = startX;
+		for (int j = 0; j < valuesCount; j++)
+		{
+			if (i != j)
+				currTerm *= (val - currX) / (xi - currX);
+			currX += delta;
+		}
+		res += currTerm;
+		xi += delta;
+	}
+
+	return res;
+}
+
+float AuxPort::Interpolation::newtonInterpolate(float val)
+{
+	AuxAssert(val >= startX && val <= endX, "Given val should lie in the range [start, end]!");
+
+	std::vector<float> dividedDifferences = yValues;
+	for (int i = 1; i < dividedDifferences.size(); i++)
+	{
+		for (int j = dividedDifferences.size() - 1; j >= i; j--)
+		{
+			dividedDifferences[j] = (dividedDifferences[j] - dividedDifferences[j - 1]) / (i * delta);
+		}
+	}
+
+	float res = dividedDifferences[0];
+	float factor = 1.0f;
+	float xi = startX;
+	for (int i = 1; i < dividedDifferences.size(); i++)
+	{
+		factor *= (val - xi);
+		res += dividedDifferences[i] * factor;
+		xi += delta;
+	}
+
+	return res;
+}
+
+size_t AuxPort::Interpolation::findIndex(float xVal)
+{
+	size_t index = (xVal - startX) / delta;
+	AuxAssert(index < yValues.size() - 1 && index >= 0, "Given val should lie between two defined xValues!");
+	return index;
+}
---
+++ AuxPort/Core/Utility/AuxMath.h
@@ -0,0 +1,134 @@
+#ifndef AUXPORT_MATH_H
+#define AUXPORT_MATH_H
+/*
+*			AuxPort Library
+			"Modules for Audio Software Development" - inpinseptipin
+
+			BSD 3-Clause License
+
+			Copyright (c) 2023, Satyarth Arora
+			All rights reserved.
+
+			Redistribution and use in source and binary forms, with or without
+			modification, are permitted provided that the following conditions are met:
+
+			1. Redistributions of source code must retain the above copyright notice, this
+			   list of conditions and the following disclaimer.
+
+			2. Redistributions in binary form must reproduce the above copyright notice,
+			   this list of conditions and the following disclaimer in the documentation
+			   and/or other materials provided with the distribution.
+
+			3. Neither the name of the copyright holder nor the names of its
+			   contributors may be used to endorse or promote products derived from
+			   this software without specific prior written permission.
+
+			THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+			AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+			IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+			DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+			FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+			DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+			SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+			CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+			OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+			OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*===================================================================================*/
+#include <vector>
+#include <complex>
+#include <cmath>
+#include "../Env/AuxEnv.h"
+#include "AuxUtility.h"
+
+namespace AuxPort
+{
+	template<class errorVal>
+	class Error
+	{
+	public:
+		enum class Type { Mean, Absolute, Relative, RMSE };
+		
+		Error()
+		{
+			this->type = Type::Absolute;
+		}
+
+		~Error() = default;
+		
+		Error(const Error& error) = default;
+		
+		void setType(const Type& type)
+		{
+			this->type = type;
+		}
+
+		float compute(const std::vector<errorVal>& calculatedValues, const std::vector<errorVal>& expectedValues)
+		{
+			AuxAssert(calculatedValues.size() == expectedValues.size(), "Vectors must be of same size!");
+
+			float result = 0.0f;
+			size_t n = calculatedValues.size();
+
+			switch (type) 
+			{
+			case Type::Mean:
+				for (size_t i = 0; i < n; i++) 
+					result += (calculatedValues[i] - expectedValues[i]);
+				result /= n;
+				break;
+
+			case Type::Absolute:
+				for (size_t i = 0; i < n; i++)
+					result += std::abs(calculatedValues[i] - expectedValues[i]);
+				result /= n;
+				break;
+
+			case Type::Relative:
+				for (size_t i = 0; i < n; i++)
+					result += std::abs(calculatedValues[i] - expectedValues[i]) / std::abs(expectedValues[i]);
+				result /= n;
+				break;
+
+			case Type::RMSE:
+				for (size_t i = 0; i < n; i++) 
+					result += std::norm((calculatedValues[i] - expectedValues[i]));
+				result = std::sqrt((float)result / n);
+				break;
+			}
+			return result;
+		}
+	private:
+		Type type;
+	};
+
+	class Interpolation
+	{
+	public:
+		enum class Type { Linear, Cubic, Cosine, Lagrange, Newton };
+		Interpolation();
+		~Interpolation() = default;
+		Interpolation(const Interpolation& interpolation) = default;
+		void setType(Type type);
+		void setXValues(float start, float end, float delta);
+		void setYValues(const std::vector<float> yValues);
+		float polate(float val);
+	private:
+		Type type;
+		float delta;
+		float startX, endX;
+		size_t valuesCount;
+		std::vector<float> yValues;
+
+		float linearInterpolate(float val);
+		float cubicInterpolate(float val);
+		float cosineInterpolate(float val);
+		float lagrangeInterpolate(float val);
+		float newtonInterpolate(float val);
+		size_t findIndex(float xVal);
+	};
+}
+
+#endif
