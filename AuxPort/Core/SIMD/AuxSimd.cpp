f4f3671dfe057659cfefe8b70fe85e6d2e997110
inpinseptipin <saty@satyartharora.com>
2024-09-03 18:25:35 -0400

    Neon 128bit support added

--- AuxPort/Core/SIMD/AuxSimd.cpp
+++ AuxPort/Core/SIMD/AuxSimd.cpp
@@ -8,7 +8,7 @@ void AuxPort::Simd::Float256::add(std::vector<float>& result, const std::vector<
 	AuxAssert(vec1.size() % 8 == 0, "Vectors should be sizes of 8");
 
 	auto noOfIterations = vec1.size() / 8u;
-	for (uint32_t i = 0; i < noOfIterations; i += 8u)
+	for (uint32_t i = 0; i < noOfIterations; i++)
 	{
 		auto register1 = _mm256_loadu_ps(vec1.data() + 8 * i);
 		auto register2 = _mm256_loadu_ps(vec2.data() + 8 * i);
@@ -27,7 +27,7 @@ void AuxPort::Simd::Float256::multiply(std::vector<float>& result,const std::vec
 	AuxAssert(vec1.size() % 8 == 0, "Vectors should be sizes of 8");
 	
 	auto noOfIterations = vec1.size() / 8u;
-	for (uint32_t i = 0; i < noOfIterations; i += 8u)
+	for (uint32_t i = 0; i < noOfIterations; i++)
 	{
 		auto register1 = _mm256_loadu_ps(vec1.data() + 8 * i);
 		auto register2 = _mm256_loadu_ps(vec2.data() + 8 * i);
@@ -88,13 +88,27 @@ void AuxPort::Simd::Float128::add(std::vector<float>& result, const std::vector<
 	AuxAssert(vec1.size() % 4 == 0, "Vectors should be sizes of 4");
 
 	auto noOfIterations = vec1.size() / 4u;
-	for (uint32_t i = 0; i < noOfIterations; i += 4u)
+	for (uint32_t i = 0; i < noOfIterations; i++)
 	{
 		auto register1 = _mm_load_ps(vec1.data() + 4 * i);
 		auto register2 = _mm_load_ps(vec2.data() + 4 * i);
 		auto sum = _mm_add_ps(register1, register2);
 		_mm_storeu_ps(result.data() + 4 * i, sum);
 	}
+#elif AUXSIMD && AUXNEON
+	AuxAssert(result.size() == vec1.size(),"Result Vector should be the same size as the Addends");
+	AuxAssert(vec1.size() == vec2.size(),"The sizes of the Addends have to be the same");
+	AuxAssert(vec1.size() % 4 == 0,"Vectors should be sizes of 4");
+
+	auto noOfInterations = vec1.size() /4u;
+	for (uint32_t i = 0; i < noOfInterations; i++)
+	{
+		auto register1 = vld1q_f32(vec1.data() + 4 * i);
+		auto register2 = vld1q_f32(vec2.data() + 4 * i);
+		auto sum = vaddq_f32(register1,register2);
+		vst1q_f32(result.data() + 4 * i, sum);
+	}
+
 #else
 	AuxPort::Logger::Log("SIMD Instruction set not available");
 #endif
@@ -107,12 +121,25 @@ void AuxPort::Simd::Float128::multiply(std::vector<float>& result, const std::ve
 	AuxAssert(vec1.size() % 4 == 0, "Vectors should be sizes of 4");
 
 	auto noOfIterations = vec1.size() / 4u;
-	for (uint32_t i = 0; i < noOfIterations; i += 4u)
+	for (uint32_t i = 0; i < noOfIterations; i++)
 	{
 		auto register1 = _mm_load_ps(vec1.data() + 4 *  i);
 		auto register2 = _mm_load_ps(vec2.data() + 4 *  i);
 		auto product = _mm_mul_ps(register1, register2);
 		_mm_storeu_ps(result.data() + 4 * i, product);
+	}
+#elif AUXSIMD && AUXNEON
+	AuxAssert(result.size() == vec1.size(),"Result Vector should be the same size as the Addends");
+	AuxAssert(vec1.size() == vec2.size(),"The sizes of the Addends have to be the same");
+	AuxAssert(vec1.size() % 4 == 0,"Vectors should be sizes of 4");
+
+	auto noOfInterations = vec1.size() /4u;
+	for (uint32_t i = 0; i < noOfInterations; i++)
+	{
+		auto register1 = vld1q_f32(vec1.data() + 4 * i);
+		auto register2 = vld1q_f32(vec2.data() + 4 * i);
+		auto sum = vmulq_f32(register1,register2);
+		vst1q_f32(result.data() + 4 * i, sum);
 	}
 #else
 	AuxPort::Logger::Log("SIMD Instruction set not available");
@@ -132,6 +159,18 @@ void AuxPort::Simd::Float128::sqrt(std::vector<float>& result, const std::vector
 		auto register1 = _mm_loadu_ps(vec.data() + 4 * i);
 		auto squareRoot = _mm_sqrt_ps(register1);
 		_mm_storeu_ps(result.data() + 4 * i, squareRoot);
+	}
+#elif AUXSIMD && AUXNEON
+	AuxAssert(AuxPort::Env::supportsNeon(), "The current processor does not support Neon and hence 128-bit SIMD instructions cannot be performed.");
+	AuxAssert(result.size() == vec.size(), "The sizes of the Input Vector and the Result Vector have to be the same");
+	AuxAssert(vec.size() % 4 == 0, "Vectors should be sizes of 4");
+
+	auto noOfIterations = vec.size() / 4u;
+	for (uint32_t i = 0; i < noOfIterations; i++)
+	{
+		auto register1 = vld1q_f32(vec.data() + 4 * i);
+		auto squareRoot = vsqrtq_f32(register1);
+		vst1q_f32(result.data() + 4 * i, squareRoot);
 	}
 #else
 	AuxPort::Logger::Log("SIMD Instruction set not available");
@@ -153,6 +192,20 @@ void AuxPort::Simd::Float128::fma(std::vector<float>& result, const std::vector<
 		auto register3 = _mm_loadu_ps(vec3.data() + 4 * i);
 		auto ans = _mm_fmadd_ps(register1, register2, register3);
 		_mm_storeu_ps(result.data() + 4 * i, ans);
+	}
+#elif AUXSIMD && AUXNEON
+	AuxAssert(vec1.size() == vec2.size() && vec2.size() == vec3.size() && result.size() == vec1.size(), "The sizes of the Input Vectors and the Result Vector have to be the same");
+	AuxAssert(vec1.size() == vec2.size() && vec2.size() == vec3.size() && result.size() == vec1.size(), "The sizes of the Input Vectors and the Result Vector have to be the same");
+	AuxAssert(vec1.size() % 4 == 0, "Vectors should be sizes of 4");
+
+	auto noOfIterations = vec1.size() / 4u;
+	for (uint32_t i = 0; i < noOfIterations; i++)
+	{
+		auto register1 = vld1q_f32(vec1.data() + 4 * i);
+		auto register2 = vld1q_f32(vec2.data() + 4 * i);
+		auto register3 = vld1q_f32(vec3.data() + 4 * i);
+		auto ans = vfmaq_f32(register1, register2, register3);
+		vst1q_f32(result.data() + 4 * i, ans);
 	}
 #else
 	AuxPort::Logger::Log("SIMD Instruction set not available");
