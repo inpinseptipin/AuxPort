3930555fbc12d88f0ff6d14dc054883db3d70865
inpinseptipin <saty@satyartharora.com>
2025-04-04 13:36:37 -0400

    Aux Bench Master

--- AuxBench/Audio/Oscillators/OscillatorBench.cpp
+++ AuxBench/Audio/Oscillators/OscillatorBench.cpp
@@ -2,7 +2,7 @@
 #include "OscillatorBench.h"
 #include "OscillatorBench.h"
 
-void AuxPort::Benchmarks::OscillatorBench::setOscillators(const std::vector<AuxPort::Audio::Oscillator> oscillators, const std::vector<std::string> names)
+void AuxPort::Benchmarks::Oscillator::setOscillators(const std::vector<AuxPort::Audio::Oscillator> oscillators, const std::vector<std::string> names)
 {
 	this->oscillators = oscillators;
 	this->oscillatorNames = names;
@@ -10,7 +10,7 @@ void AuxPort::Benchmarks::OscillatorBench::setOscillators(const std::vector<AuxP
 	std::fill(benchmarkTimes.begin(), benchmarkTimes.end(), 0.0f);
 }
 
-void AuxPort::Benchmarks::OscillatorBench::benchmark()
+void AuxPort::Benchmarks::Oscillator::benchmark()
 {
 	auto sample = 0.0f;
 	for (uint32_t i = 0; i < oscillators.size(); i++)
@@ -23,28 +23,79 @@ void AuxPort::Benchmarks::OscillatorBench::benchmark()
 				sample = oscillators[i].process();
 			}
 			stop();
-			benchmarkTimes[i] += getEllapsedTime();
-
+			benchmarkTimes[i] += getEllapsedTime(AuxPort::Timer::nano);
 		}
+		benchmarkTimes[i] /= numberOfIterations;
 	}
 }
 
-void AuxPort::Benchmarks::OscillatorBench::setParameters(size_t bufferSize, size_t numberOfIterations)
+void AuxPort::Benchmarks::Oscillator::setParameters(size_t bufferSize, size_t numberOfIterations)
 {
 	this->bufferSize = bufferSize;
 	this->numberOfIterations = numberOfIterations;
 }
 
-void AuxPort::Benchmarks::OscillatorBench::Log()
+void AuxPort::Benchmarks::Oscillator::Log()
 {
 	setColour(AuxPort::ColourType::Green);
-	std::cout << "\n\n*************Benchmarks**********\n";
+	std::cout << "\n\n*************Benchmarks**********\n\n";
 	std::cout << "Buffer Size : " << bufferSize << "\n";
 	std::cout << "Number of Iterations : " << numberOfIterations << "\n\n";
 	for (uint32_t i = 0; i < oscillators.size(); i++)
 	{
 		std::cout << "Oscillator Name : " << oscillatorNames[i] << "\n";
-		std::cout << "Average Benchmark Time : " << benchmarkTimes[i] << "\n\n";
+		std::cout << "Average Benchmark Time : " << benchmarkTimes[i] << "ns" << "\n\n";
 	}
 	setColour(AuxPort::ColourType::White);
+}
+
+void AuxPort::Benches::OscillatorBench::benchmarkSines(size_t bufferSize, size_t numberOfIterations)
+{
+	AuxPort::Benchmarks::Oscillator oscillatorBench;
+	std::vector<AuxPort::Audio::Oscillator> oscillators;
+	oscillators.push_back(AuxPort::Audio::Sine::FastSine());
+	oscillators.push_back(AuxPort::Audio::Sine::Sine());
+	oscillators.push_back(AuxPort::Audio::Sine::AuxSine());
+	oscillators.push_back(AuxPort::Audio::Sine::AuxSine2());
+	oscillators.push_back(AuxPort::Audio::Sine::BhaskaraSine());
+	oscillators.push_back(AuxPort::Audio::Sine::JavidX9Sine());
+	oscillators.push_back(AuxPort::Audio::Sine::JavidX9Sine2());
+	oscillators.push_back(AuxPort::Audio::Sine::ParabolicSine());
+
+	oscillatorBench.setParameters(bufferSize, numberOfIterations);
+	oscillatorBench.setOscillators(oscillators, { "fastsine","std::sin","AuxSine","AuxSine2","Bhasakara Sine","JavidX9","Javidx92","Parabolic"});
+	oscillatorBench.benchmark();
+	oscillatorBench.Log();
+}
+
+void AuxPort::Benches::OscillatorBench::benchmarkSawtooths(size_t bufferSize, size_t numberOfIterations)
+{
+	AuxPort::Benchmarks::Oscillator oscillatorBench;
+	std::vector<AuxPort::Audio::Oscillator> oscillators;
+	oscillators.push_back(AuxPort::Audio::Sawtooth::BipolarSawtooth());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::DPWSaw());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::PBSaw());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::PBWSaw());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::SawSin1());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::SawSin2());
+	oscillators.push_back(AuxPort::Audio::Sawtooth::UnipolarSawtooth());
+	oscillatorBench.setParameters(bufferSize, numberOfIterations);
+	oscillatorBench.setOscillators(oscillators, { "Bipolar Sawtooth","DPW","PBSaw","PBWSaw","SawSin1","SawSin2","Unipolar Sawtooth" });
+	oscillatorBench.benchmark();
+	oscillatorBench.Log();
+
+
+}
+
+void AuxPort::Benches::OscillatorBench::benchmarkTriangle(size_t bufferSize, size_t numberOfIterations)
+{
+	AuxPort::Benchmarks::Oscillator oscillatorBench;
+	std::vector<AuxPort::Audio::Oscillator> oscillators;
+	oscillators.push_back(AuxPort::Audio::Triangle::DPWTriangle1());
+	oscillators.push_back(AuxPort::Audio::Triangle::Triangle());
+
+	oscillatorBench.setParameters(bufferSize, numberOfIterations);
+	oscillatorBench.setOscillators(oscillators, { "DPW","Triangle" });
+	oscillatorBench.benchmark();
+	oscillatorBench.Log();
 }
--- AuxBench/Audio/Oscillators/OscillatorBench.h
+++ AuxBench/Audio/Oscillators/OscillatorBench.h
@@ -1,8 +1,8 @@
 #ifndef OSCILLATOR_BENCH_H
 #define OSCILLATOR_BENCH_H
 /*
-*			AuxTest
-			"Test Test Test" - inpinseptipin
+*			AuxBench
+			"Bench Bench Bench" - inpinseptipin
 
 			BSD 3-Clause License
 
@@ -46,21 +46,83 @@ namespace AuxPort
 {
 	namespace Benchmarks
 	{
-		class OscillatorBench : public AuxPort::Bench,AuxPort::Timer
+		class Oscillator : public AuxPort::Bench,AuxPort::Timer
 		{
 		public:
-			OscillatorBench() = default;
-			~OscillatorBench() = default;
-			OscillatorBench(const OscillatorBench& oscillatorBench) = default;
+			Oscillator() = default;
+			~Oscillator() = default;
+			Oscillator(const Oscillator& oscillator) = default;
+			/**
+			  @brief Sets the vector of oscillators and their names for benchmarking and logging 
+			  @param oscillators
+			  @param names
+			  Example Implementation
+			  \code{.cpp}
+			  	AuxPort::Benchmarks::Oscillator oscillatorBench;
+				std::vector<AuxPort::Audio::Oscillator> oscillators;
+				oscillators.push_back(AuxPort::Audio::Sine::FastSine());
+				oscillatorBench.setOscillators(oscillators,{"FastSine"});
+			  \endcode 
+			 */
 			void setOscillators(const std::vector<AuxPort::Audio::Oscillator> oscillators, const std::vector<std::string> names);
+			/**
+			  @brief [Pure Virtual Function] Overridable this method to write your benchmarking scheme.
+			  Example Definition
+			  \code{.cpp}
+			   void benchmark()
+			   {
+					auto sample = 0.0f;
+					for (uint32_t i = 0; i < oscillators.size(); i++)
+					{
+						for (uint32_t j = 0; j < numberOfIterations; j++)
+						{
+							start();
+							for (uint32_t k = 0; k < bufferSize; k++)
+							{
+								sample = oscillators[i].process();
+							}
+							stop();
+							benchmarkTimes[i] += getEllapsedTime(AuxPort::Timer::nano);
+						}
+						benchmarkTimes[i] /= numberOfIterations;
+					}
+			   }
+			  \endcode 
+			 */
 			void benchmark() override;
+			/**
+			  @brief sets benchmark parameters 
+			  @param bufferSize
+			  @param numberOfIterations
+			  Example Implementation
+			  \code{.cpp}
+				AuxPort::Benchmarks::Oscillator oscillatorBench;
+				oscillatorBench.setParameters(1024,1000);
+			  \endcode 
+			 */
 			void setParameters(size_t bufferSize = 1024, size_t numberOfIterations = 1000);
+			/**
+			  @brief [Virtual Function] Override the function, to log the internal oscillator state.
+			 */
 			void Log() override;
 		protected:
 			std::vector<AuxPort::Audio::Oscillator> oscillators;
 			std::vector<std::string> oscillatorNames;
 			size_t bufferSize = 1024;
 			size_t numberOfIterations = 1000;
+		};
+		
+	}
+
+	namespace Benches
+	{
+
+		class OscillatorBench
+		{
+		public:
+			static void benchmarkSines(size_t bufferSize, size_t numberOfIterations);
+			static void benchmarkSawtooths(size_t bufferSize, size_t numberOfIterations);
+			static void benchmarkTriangle(size_t bufferSize, size_t numberOfIterations);
 		};
 	}
 
