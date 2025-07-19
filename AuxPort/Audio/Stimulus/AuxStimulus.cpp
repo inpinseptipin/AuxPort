12b7eed6be67baeef3a28118ec57e6a18e77c7e5
inpinseptipin <saty@satyartharora.com>
2025-07-19 14:17:03 -0700

    ESS added

--- AuxPort/Audio/Stimulus/AuxStimulus.cpp
+++ AuxPort/Audio/Stimulus/AuxStimulus.cpp
@@ -1,4 +1,5 @@
 #include "AuxStimulus.h"
+#include "../../Core/Utility/AuxUtility.h"
 
 std::vector<float> AuxPort::Audio::Stimulus::generateImpulse(float lengthInTime,float sampleRate)
 {
@@ -10,3 +11,43 @@ std::vector<float> AuxPort::Audio::Stimulus::generateImpulse(float lengthInTime,
     return impulse;
     
 }
+
+std::vector<float> AuxPort::Audio::Stimulus::generateImpulse(size_t numberOfSamples)
+{
+    AuxAssert(numberOfSamples > 0 && numberOfSamples < 132000, "Number of Samples has to be greater than zero");
+    std::vector<float> impulse;
+    impulse.resize(numberOfSamples);
+    impulse[0] = 1.0f;
+    return impulse;
+}
+
+std::vector<float> AuxPort::Audio::Stimulus::generateWhiteNoise(float lengthInTime, float sampleRate)
+{
+    AuxAssert(lengthInTime > 0.0f, "Length in Time cannot be negative");
+    size_t numberOfSamples = static_cast<size_t>(lengthInTime * sampleRate);
+    return AuxPort::Utility::generateRandomValues<float>(numberOfSamples);
+}
+
+std::vector<float> AuxPort::Audio::Stimulus::generateWhiteNoise(size_t numberOfSamples)
+{
+    AuxAssert(numberOfSamples > 0 && numberOfSamples < UINT_LEAST32_MAX, "Number of Samples has to be greater than zero");
+    return AuxPort::Utility::generateRandomValues<float>(numberOfSamples);
+}
+
+std::vector<float> AuxPort::Audio::Stimulus::generateExponentialSineSweep(float lengthInTime, float startFrequency,float endFrequency,float sampleRate)
+{
+    AuxAssert(lengthInTime > 0.0f, "Length in time cannot be negative");
+    size_t numberOfSamples = static_cast<size_t>(lengthInTime * sampleRate);
+    std::vector<float> stimulus;
+    stimulus.resize(numberOfSamples);
+    auto L = lengthInTime / log(endFrequency / startFrequency);
+    auto mod = 0.0f;
+    auto inc = 1.0f / sampleRate;
+    for (uint32_t i = 0; i < numberOfSamples; i++)
+    {
+        stimulus[i] = sin(2 * pi * startFrequency * L * (expf(mod / L) - 1));
+        mod += inc;
+    }
+    return stimulus;
+}
+
--- AuxPort/Audio/Stimulus/AuxStimulus.h
+++ AuxPort/Audio/Stimulus/AuxStimulus.h
@@ -8,10 +8,45 @@ namespace AuxPort
 {
 	namespace Audio
 	{
+		/**
+		 Class consisting of static methods to generate Stimuli for various audio applications
+		 */
 		class Stimulus
 		{
 		public:
+			/**
+			  @brief Generates an impulse
+			  @param lengthInTime
+			  @param sampleRate
+			  @return 
+			 */
 			static std::vector<float> generateImpulse(float lengthInTime,float sampleRate);
+			/**
+			  @brief Generates an Impulse
+			  @param numberOfSamples
+			  @return 
+			 */
+			static std::vector<float> generateImpulse(size_t numberOfSamples);
+			/**
+			  @brief Generates White Noise 
+			  @param lengthInTime
+			  @param sampleRate
+			  @return 
+			 */
+			static std::vector<float> generateWhiteNoise(float lengthInTime, float sampleRate);
+			/**
+			  @brief Generates White Noise 
+			  @param numberOfSamples
+			  @return 
+			  \code{.cpp}
+			  
+			  \endcode 
+			 */
+			static std::vector<float> generateWhiteNoise(size_t numberOfSamples);
+
+			static std::vector<float> generateExponentialSineSweep(float lengthInTime, float startFrequency, float endFrequency, float sampleRate);
+
+
 		};
 	}
 }
