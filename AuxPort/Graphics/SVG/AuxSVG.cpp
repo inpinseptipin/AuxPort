c129a06eeeab725cf87a9184db4206c91cd21fc4
inpinseptipin <saty@satyartharora.com>
2025-11-27 04:09:36 -0500

    Abstract Object and SVG renderer

--- AuxPort/Graphics/SVG/AuxSVG.cpp
+++ AuxPort/Graphics/SVG/AuxSVG.cpp
@@ -1,110 +1,136 @@
 #include "AuxSVG.h"
 
-AuxPort::Graphics::SVG::SVG()
+AuxPort::Graphics::SVG::SVG::SVG()
 {
 	svgData.clear();
 }
 
-bool AuxPort::Graphics::SVG::open(const std::string& fileName, const Mode& mode, bool log)
-{
-	if (mode == Mode::Read)
-	{
-		if (log)
-			AuxPort::Logger::Log("Trying to Open " + fileName);
-		try
-		{
-			fileReader.reset(new std::ifstream(fileName));
-			if (log)
-			{
-				if (fileReader->is_open())
-					Logger::Log("Successfully Opened " + fileName);
-			}
-			this->fileName = fileName;
-			auto dotIndex = fileName.find('.');
-			this->fileExtension = fileName.substr(dotIndex + 1, fileName.size() - dotIndex);
-		}
-		catch (std::ifstream::failure failure)
-		{
-			AuxPort::Logger::Log("Failed to open " + fileName, AuxPort::LogType::Error, AuxPort::ColourType::Red);
-		}
-
-		this->mode = mode;
-		return fileReader->is_open();
-	}
-	else if (mode == Mode::Write)
-	{
-		if (log)
-			AuxPort::Logger::Log("Trying to Open " + fileName); //char(28)
-		try
-		{
-			fileWriter.reset(new std::ofstream(fileName, std::ofstream::out));
-			auto open = fileWriter->is_open();
-			if (log)
-			{
-				open == true ? AuxPort::Logger::Log(fileName + " opened successfully", AuxPort::LogType::Success, AuxPort::ColourType::Green) :
-					AuxPort::Logger::Log("Failed to open " + fileName, AuxPort::LogType::Error, AuxPort::ColourType::Red);
-			}
-			this->mode = mode;
-			this->fileName = fileName;
-			auto dotIndex = fileName.find('.');
-			this->fileExtension = fileName.substr(dotIndex + 1, fileName.size() - dotIndex);
-		}
-		catch (std::ofstream::failure fail)
-		{
-			AuxPort::Logger::Log("Failed to open " + fileName, AuxPort::LogType::Error, AuxPort::ColourType::Red);
-		}
-		svgData+="<svg width=\"800\" height=\"300\"  xmlns=\"http://www.w3.org/2000/svg\">";
-		return fileWriter->is_open();
-	}
-	else if (mode == Mode::Update)
-	{
-		if (log)
-			AuxPort::Logger::Log("Trying to Open " + fileName); //char(28)
-		try
-		{
-			fileWriter.reset(new std::ofstream(fileName, std::ofstream::out | std::ofstream::app));
-			auto open = fileWriter->is_open();
-			if (log)
-			{
-				open == true ? AuxPort::Logger::Log(fileName + " opened successfully", AuxPort::LogType::Success, AuxPort::ColourType::Green) :
-					AuxPort::Logger::Log("Failed to open " + fileName, AuxPort::LogType::Error, AuxPort::ColourType::Red);
-			}
-			this->mode = mode;
-			this->fileName = fileName;
-			auto dotIndex = fileName.find('.');
-			this->fileExtension = fileName.substr(dotIndex + 1, fileName.size() - dotIndex);
-		}
-		catch (std::ofstream::failure fail)
-		{
-			AuxPort::Logger::Log("Failed to open " + fileName, AuxPort::LogType::Error, AuxPort::ColourType::Red);
-		}
-		return fileWriter->is_open();
-	}
-	else
-	{
-		return false;
-	}
-}
-
-
-void AuxPort::Graphics::SVG::addBackground(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+void AuxPort::Graphics::SVG::SVG::addBackground(AuxPort::Graphics::SVG::Colour& colour)
 {
 	backgroundData.clear();
-	backgroundData += "<rect width=\"100%\" height=\"100%\" fill=\"rgba(" + std::to_string(red) + "," + std::to_string(green) + "," + std::to_string(blue) + ","  + std::to_string(alpha) + ")\"/>";
-
+	colour.generateString();
+	backgroundData += "<rect width=\"100%\" height=\"100%\" fill=" + colour.getSVGString() + "/>";
 }
 
-void AuxPort::Graphics::SVG::addText(const std::string& text, float startX, float startY, float fontSize)
+void AuxPort::Graphics::SVG::SVG::addText(const std::string& text, float startX, float startY, float fontSize)
 {
 	textData.push_back("<text x=\"" + std::to_string(startX) + "\" y=\"" + std::to_string(startY) + "\"" + ">" + text + "</text>");
 }
 
-void AuxPort::Graphics::SVG::save()
+void AuxPort::Graphics::SVG::SVG::addText(AuxPort::Graphics::SVG::Text& text)
 {
+	text.generateString();
+	textData.push_back(text.getSVGString());
+}
+
+void AuxPort::Graphics::SVG::SVG::save()
+{
+	svgData += "<svg width=\"800\" height=\"300\"  xmlns=\"http://www.w3.org/2000/svg\">";
 	svgData += backgroundData;
 	for (uint32_t i = 0;i < textData.size();i++)
 		svgData += textData[i];
 	svgData += "</svg>";
 	writeLineToFile(svgData);
 	this->close();
+}
+
+AuxPort::Graphics::SVG::Colour::Colour()
+{
+	red = 255;
+	green = 255;
+	blue = 255;
+	alpha = 255;
+}
+
+AuxPort::Graphics::SVG::Colour::Colour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+{
+	this->red = red;
+	this->green = green;
+	this->blue = blue;
+	this->alpha = alpha;
+}
+
+uint8_t AuxPort::Graphics::SVG::Colour::getRed()
+{
+	return red;
+}
+
+uint8_t AuxPort::Graphics::SVG::Colour::getGreen()
+{
+	return green;
+}
+
+uint8_t AuxPort::Graphics::SVG::Colour::getBlue()
+{
+	return blue;
+}
+
+uint8_t AuxPort::Graphics::SVG::Colour::getAlpha()
+{
+	return alpha;
+}
+
+void AuxPort::Graphics::SVG::Colour::setColour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+{
+	this->red = red;
+	this->green = green;
+	this->blue = blue;
+	this->alpha = alpha;
+}
+
+void AuxPort::Graphics::SVG::Colour::generateString()
+{
+	svgString.clear();
+	svgString += "\"rgba(" + std::to_string(red) + "," + std::to_string(green) + "," + std::to_string(blue) + "," + std::to_string(alpha) + ")\" ";
+}
+
+void AuxPort::Graphics::SVG::Text::setText(const std::string& text)
+{
+	this->text = text;
+}
+
+void AuxPort::Graphics::SVG::Text::setFontSize(float fontSize)
+{
+	this->fontSize = fontSize;
+}
+
+void AuxPort::Graphics::SVG::Text::setXY(float startX, float startY)
+{
+	this->startX = startX;
+	this->startY = startY;
+}
+
+void AuxPort::Graphics::SVG::Text::setFill(const Colour& fillColour)
+{
+	this->fillColour = fillColour;
+}
+
+void AuxPort::Graphics::SVG::Text::setStroke(const Colour& strokeColour)
+{
+	this->strokeColour = strokeColour;
+}
+
+void AuxPort::Graphics::SVG::Text::generateString()
+{
+	svgString.clear();
+	strokeColour.generateString();
+	fillColour.generateString();
+	svgString += "<text";
+	svgString += valueToString("x", startX);
+	svgString += valueToString("y", startY);
+	svgString += "fill=" + fillColour.getSVGString();
+	svgString += "stroke=" + strokeColour.getSVGString();
+	svgString += ">";
+	svgString += text;
+	svgString += "</text>";
+}
+
+std::string& AuxPort::Graphics::SVG::SVGObject::getSVGString()
+{
+	return svgString;
+}
+
+std::string AuxPort::Graphics::SVG::SVGObject::valueToString(const std::string& key, float val)
+{
+	return " " + key + "=\"" + std::to_string(val) + "\" ";
 }
--- AuxPort/Graphics/SVG/AuxSVG.h
+++ AuxPort/Graphics/SVG/AuxSVG.h
@@ -7,45 +7,130 @@ namespace AuxPort
 {
 	namespace Graphics
 	{
-		/**
-			Scalable Vector Graphic Engine (Creates/Reads an SVG file)
-		*/
-		class SVG : protected AuxPort::TextFile
+		namespace SVG
 		{
-		public:
-			SVG();
-			~SVG() = default;
-			SVG(const SVG& svg) = default;
-			bool open(const std::string& fileName, const Mode& mode = Mode::Read, bool log = false) override;
-			void save();
 /**
-   @brief Adds Background to the SVG file
+	SVG Abstract Object
+*/
+			class SVGObject
+			{
+			public:
+				SVGObject() = default;
+				~SVGObject() = default;
+				SVGObject(const SVGObject& svgObject) = default;
+/**
+   @brief Returns the SVG String for the particular object
+   @return 
+*/
+				std::string& getSVGString();
+			protected:
+/**
+   @brief Generates the appropriate SVG string for the object [Overridable]
+*/
+				virtual void generateString() = 0;
+				static std::string valueToString(const std::string& key,float val);
+				std::string svgString;
+			};
+
+/**
+	Creates a Colour Object [Supports 8 bit RGBA]
+*/
+			class Colour : public SVGObject
+			{
+			public:
+				Colour();
+				Colour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
+				~Colour() = default;
+				Colour(const Colour& colour) = default;
+				uint8_t getRed();
+				uint8_t getGreen();
+				uint8_t getBlue();
+				uint8_t getAlpha();
+/**
+   @brief Sets a new RGBA colour
    @param red
    @param green
    @param blue
    @param alpha
-   @details
-   Example Definition/Implementation
-   \code{.cpp}
+*/
+				void setColour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
+/**
+   @brief Generates the appropriate SVG string for the Colour Object
+*/
+				void generateString() override;
+			protected:
+				uint8_t red;
+				uint8_t green;
+				uint8_t blue;
+				uint8_t alpha;
+			};
+
+/**
+	Creates a Text SVG object
+*/
+			class Text : public SVGObject
+			{
+			public:
+				Text() = default;
+				~Text() = default;
+				Text(const Text& text) = default;
+				void setText(const std::string& text);
+				void setFontSize(float fontSize);
+				void setXY(float startX, float startY);
+				void setFill(const Colour& fillColour);
+				void setStroke(const Colour& strokeColour);
+				void generateString() override;
+			protected:
+				std::string text;
+				float fontSize;
+				float startX;
+				float startY;
+				Colour fillColour;
+				Colour strokeColour;
+			};
+
+
+/**
+	Scalable Vector Graphic Engine (Creates/Reads an SVG file)
+*/
+			class SVG : public AuxPort::TextFile
+			{
+			public:
+				SVG();
+				~SVG() = default;
+				SVG(const SVG& svg) = default;
+				void save();
+/**
+	@brief Adds Background to the SVG file
+	@param red
+	@param green
+	@param blue
+	@param alpha
+	@details
+	Example Definition/Implementation
+	\code{.cpp}
 		AuxPort::Graphics::SVG svg;
 		svg.addBackground(13,13,13,255);
-   \endcode 
+	\endcode
 */
-			void addBackground(uint8_t red = 13, uint8_t green = 13, uint8_t blue = 13, uint8_t alpha = 255);
+				void addBackground(AuxPort::Graphics::SVG::Colour& colour);
 /**
-   @brief Draws Text on the File
-   @param text
-   @param startX
-   @param startY
-   @param fontSize
-   @details
+	@brief Draws Text on the File
+	@param text
+	@param startX
+	@param startY
+	@param fontSize
+	@details
 */
-			void addText(const std::string& text, float startX, float startY, float fontSize);
-		protected:
-			std::string svgData;
-			std::string backgroundData;
-			std::vector<std::string> textData;
-		};
+				void addText(const std::string& text, float startX, float startY, float fontSize);
+				void addText(AuxPort::Graphics::SVG::Text& text);
+			protected:
+				std::string svgData;
+				std::string backgroundData;
+				std::vector<std::string> textData;
+			};
+
+		}
 	}
 }
 
