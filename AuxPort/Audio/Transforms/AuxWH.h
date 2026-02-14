446438a133a51f72227762de9a4aabeed067ad8d
inpinseptipin <saty@satyartharora.com>
2026-02-14 14:29:34 -0500

    Doc Update

--- AuxPort/Audio/Transforms/AuxWH.h
+++ AuxPort/Audio/Transforms/AuxWH.h
@@ -9,15 +9,43 @@ namespace AuxPort
 {
 	namespace Audio
 	{
+		/**
+		 * Implementation of the Fast Walsh Hadamard Transform
+		 */
 		class WalshHadamardTransform
 		{
 		public:
+			/**
+			  @brief Instantiates the Fast Walsh Hadamard Transform Engine with the size 
+			  @param whtSize
+			  \code{.cpp}
+				AuxPort::Audio::WalshHadamardTransform wht(512);
+			  \endcode 
+			 */
 			WalshHadamardTransform(size_t whtSize);
 			~WalshHadamardTransform() = default;
 			WalshHadamardTransform(const WalshHadamardTransform& walshHadamardTransform) = default;
+			/**
+			  @brief Computes the forward transform
+			  @param input
+			  @param output
+			  \code{.cpp}
+			  
+			  \endcode 
+			 */
 			void transform(const std::vector<float>& input, std::vector<float>& output);
+			/**
+			  @brief Computes the inverse transform 
+			  @param output
+			  \code{.cpp}
+			  
+			  \endcode 
+			 */
 			void inverseTransform(std::vector<float>& output);
 		protected:
+			/**
+			  @brief FWHT Kernel 
+			 */
 			void compute();
 			std::vector<float> whtBuffer;
 			size_t whtSize;
